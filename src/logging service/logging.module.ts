e3e6609c77d670853a291450c8a01420ef06f550
Hussien <husseinfayed86@gmail.com>
2023-01-30 17:15:23 +0200

    loggs tries

--- src/logging service/logging.module.ts
+++ src/logging service/logging.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Log, LogSchema } from 'src/models/log.model';
+import CustomLogger from './customLogger';
 import LogsService from './logging.service';
 
 @Module({
@@ -10,8 +11,10 @@ import LogsService from './logging.service';
     ])
   ],
 
-  providers: [LogsService],
-//   controllers: [OrderController],
+  providers: [CustomLogger,LogsService],
+  exports: [CustomLogger],
+
+
 
 })
 export class LoggingModule { }
